1eb1028d863fe5aac45e1d41f2f13736e2be4a21
Markus Averstad <markus.averstad@gmail.com>
2018-05-24 13:56:36 +0200

    Add redux devtools

--- src/client/Store.js
+++ src/client/Store.js
@@ -7,7 +7,7 @@ const store = createStore(
   {
     cartReducer: {...cartReducerInitialState},
     productsReducer: {...productsReducerInitialState}
-  }
+  }, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 export default store
